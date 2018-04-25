9a757eb31bdf8e60be392b50c9cffe373d3b06d3
Yong Choi <yong.choi@semanticbits.com>
2018-04-24 21:39:52 -0400

    Got the unit tests to work

--- src/test/file/files-spec.ts
+++ src/test/file/files-spec.ts
@@ -1,17 +1,16 @@
-import { patternPaste, Settings } from '../../src/logic';
+import { patternPaste, Settings } from '../../logic';
 import { expect } from 'chai';
-const os = require('os')
-const fs = require('fs-extra')
+const fs = require('fs-extra');
 
 describe('File', () => {
     it('should generate expected result', () => {
 
         // Arrange
         try {
-            fs.copySync('test/file/inputs/category.tss', 'temp/category.tss')
-            fs.copySync('test/file/inputs/category.1.tss', 'temp/category.1.tss')            
+            fs.copySync('src/test/file/inputs/category.tss', 'temp/category.tss');
+            fs.copySync('src/test/file/inputs/category.1.tss', 'temp/category.1.tss');
         } catch (err) {
-            console.error(err)
+            console.error(err);
         }
 
         const settings: Settings = {
@@ -29,19 +28,17 @@ describe('File', () => {
 
             // Assert
             const generatedFile = fs.readFileSync('temp/expected.tss', 'utf8');
-            const expectedFile = fs.readFileSync('test/file/expected/expected.tss', 'utf8');
+            const expectedFile = fs.readFileSync('src/test/file/expected/expected.tss', 'utf8');
 
             expect(generatedFile).to.equal(expectedFile);
-            expect(generatedFile).to.not.be.null; 
+            expect(generatedFile).to.not.equal(null);
 
 
             const generatedFile2 = fs.readFileSync('temp/expected.1.tss', 'utf8');
-            const expectedFile2 = fs.readFileSync('test/file/expected/expected.1.tss', 'utf8');
+            const expectedFile2 = fs.readFileSync('src/test/file/expected/expected.1.tss', 'utf8');
 
             expect(generatedFile2).to.equal(expectedFile2);
-            expect(generatedFile2).to.not.be.null; 
-
-            
+            expect(generatedFile2).to.not.equal(null);            
         });
     });
 });
